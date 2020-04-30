df78bdab0edd684c567edc1a469556664aa2ee67
hd_li <hd_li@ctrip.com>
2020-04-30 14:39:17 +0800

    反转字符串

---
+++ src/main/java/com/lihd/java/L344.java
@@ -0,0 +1,29 @@
+package com.lihd.java;
+
+/**
+ * @program: lihd-leetcode
+ * @description:
+ * @author: li_hd
+ * @create: 2020-04-30 14:36
+ **/
+public class L344 {
+
+    public void reverseString(char[] s) {
+
+        final int length = s.length;
+
+        for (int i = 0; i < length / 2; i++) {
+
+            char tmp = s[length - 1 - i];
+
+            s[length - 1 -i] = s[i];
+
+            s[i] = tmp;
+
+        }
+
+
+    }
+
+
+}
